57ec25a4330e1d966819a95851e9242fda5e279f
guy.g <guy.g@taboola.com>
2017-01-12 08:44:58 +0200

    server - the beginning

--- search.server/src/main/java/log/LogWrapper.java
+++ search.server/src/main/java/log/LogWrapper.java
@@ -16,6 +16,14 @@ public class LogWrapper {
         logger = Logger.getLogger(clazz);
     }
 
+    public void error(String message, Object ... args) {
+        logger.error(String.format(message, args));
+    }
+
+    public void error(String message, Throwable t, Object ... args) {
+        logger.error(String.format(message, args), t);
+    }
+
     public void info(String message, Object ... args) {
         logger.info(String.format(message, args));
     }
